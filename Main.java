04437df2d36e49ae3f7b340f2c3004f28370c956
saikishore03 <31539278+saikishore03@users.noreply.github.com>
2017-09-16 22:03:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student[] students={ new Student(1,kishore,1996-10-03,50),
+				new Student(2,kishore,1996-10-03,50),
+		new Student(3,kishore,1996-10-03,50)};
+		StudentGroup g= new StudentGroup(3);
+		g.setStudents(students);
+		
+		
+	}
+
+}
